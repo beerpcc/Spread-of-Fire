992d70d7a0fecd10431128f879030ce19edf6af5
nonez- <sujin.non@mail.kmutt.ac.th>
2014-10-25 00:11:26 +0700

    test

---
+++ Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author user
+ */
+public class Test {
+    public static void main(String[] args){
+        Forest f = new Forest(10);
+        f.printArr();
+        f.fireSpread();
+        System.out.println();
+        f.printArr();
+    }
+}
